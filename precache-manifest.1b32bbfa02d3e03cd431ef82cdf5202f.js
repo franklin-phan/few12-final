d0b0e86e0c120e302eb34c804448a5fde5302356
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-05-12 14:12:41 -0500

    Updates

---
+++ precache-manifest.1b32bbfa02d3e03cd431ef82cdf5202f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6009fb3a3dcd18e6272e7ef3d7d6c41c",
+    "url": "/few12-final/index.html"
+  },
+  {
+    "revision": "8e3fd517fdbaf4dcbd18",
+    "url": "/few12-final/static/css/main.446d1057.chunk.css"
+  },
+  {
+    "revision": "56435272315f0b1bcafb",
+    "url": "/few12-final/static/js/2.e0785745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/few12-final/static/js/2.e0785745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3fd517fdbaf4dcbd18",
+    "url": "/few12-final/static/js/main.5361d938.chunk.js"
+  },
+  {
+    "revision": "fe27d55a02b0f3408c68",
+    "url": "/few12-final/static/js/runtime-main.fb007f03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/few12-final/static/media/logo.5d5d9eef.svg"
+  }
+]);
